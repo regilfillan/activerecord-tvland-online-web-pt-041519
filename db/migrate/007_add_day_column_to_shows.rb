114382b20fe75ea220052270cec645cd2b17f475
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-18 01:46:34 +0000

    Automatically backed up by Learn

---
+++ db/migrate/007_add_day_column_to_shows.rb
@@ -0,0 +1,9 @@
+class AddDayColumnToShows < ActiveRecord::Migration[5.2]
+  
+  def change
+    add_column :shows, :day, :string 
+    add_column :shows, :genre, :string 
+    add_column
+  end 
+  
+end 
